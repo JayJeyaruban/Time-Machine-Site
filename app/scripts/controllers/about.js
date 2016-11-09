8b6fd829b31b7f012bfb51a40b6e2a532ade8c12
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-11-09 15:14:55 +0000

    Generated Angular templateing using Yeoman.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name timeMachineSiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the timeMachineSiteApp
+ */
+angular.module('timeMachineSiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name timeMachineSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the timeMachineSiteApp
+ */
+angular.module('timeMachineSiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
