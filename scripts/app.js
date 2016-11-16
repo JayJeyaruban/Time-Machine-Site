488082da3a5ec10f51f33c91e290456d25e5bc45
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-11-16 23:10:53 +0000

    Routing changes. Deleted controllers.

--- scripts/app.js
+++ scripts/app.js
@@ -9,22 +9,33 @@
  * Main module of the application.
  */
 angular
-  .module('timeMachineSiteApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('timeMachineSiteApp', [
+        'ngRoute'
+    ])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .when('/background', {
+                templateUrl: 'views/background.html'
+            })
+            .when('/research', {
+                templateUrl: 'views/research.html'
+            })
+            .when('/ui', {
+                templateUrl: 'views/ui.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        $locationProvider.html5Mode(true);
+    });
