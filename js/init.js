12c0bee4e6cc319799779a7c3e4c286d20c749ae
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-12-12 11:54:05 +0000

    Empty changes.

--- js/init.js
+++ js/init.js
@@ -24,3 +24,15 @@ $(document).ready(function(){
 });
 
 $(".dropdown-button").dropdown();
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+
+// $(document).ready(function(){
+//     $('.target').pushpin({
+//         // top: 0,
+//         bottom: 1000,
+//         offset: 0
+//     });
+// });
