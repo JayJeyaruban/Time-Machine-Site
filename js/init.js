b6c76c6b172e85da0fd4a0d7fbdf6ad586c4b52b
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-12-12 00:50:34 +0000

    UI changes to research.

--- js/init.js
+++ js/init.js
@@ -18,3 +18,7 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
 });
+
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+});
